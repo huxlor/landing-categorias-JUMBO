e9eef34a44c2cc51ea502e1f0c521870252c11bb
valtergrafico <valtercacurri@gmail.com>
2019-06-21 11:36:01 -0500

    WEBPACK agrega webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+    mode: 'none',
     entry: './src/main.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
